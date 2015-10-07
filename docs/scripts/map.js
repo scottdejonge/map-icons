bb44a3a82d0373546ffe68482a3e90fdcf99b536
Scott de Jonge <dejonge.scott@gmail.com>
2015-10-07 11:32:21 +1000

    Docs example and styles

--- docs/scripts/map.js
+++ docs/scripts/map.js
@@ -15,11 +15,11 @@ function initialise() {
 function createMap() {
 
 	// Center
-	var center = new google.maps.LatLng(-33.9, 151.2);
+	var center = new google.maps.LatLng(-27.46834, 153.02365);
 
 	// Map Options		
 	var mapOptions = {
-		zoom: 11,
+		zoom: 16,
 		center: center,
 		scrollwheel: false,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -36,64 +36,90 @@ function createMap() {
 		map: map,
 		zIndex: 10,
 		title: 'Map Icons',
-		position:  new google.maps.LatLng(-33.890542, 151.274856),
+		position:  new google.maps.LatLng(-27.46577, 153.02303),
 		icon: {
-			path: SQUARE_PIN,
-			fillColor: '#00CCBB',
+			path: SQUARE,
+			fillColor: '#6331AE',
 			fillOpacity: 1,
 			strokeColor: '',
-			strokeWeight: 0,
-			scale: 1/2
+			strokeWeight: 0
 		},
-		map_icon_label: '<span class="map-icon map-icon-walking"></span>'
+		map_icon_label: '<span class="map-icon map-icon-point-of-interest"></span>'
 	});
 	
 	var marker2 = new Marker({
 		map: map,
 		zIndex: 20,
 		title: 'Map Icons',
-		position:  new google.maps.LatLng(-33.923036, 151.259052),
+		position:  new google.maps.LatLng(-27.46617, 153.01865),
 		icon: {
 			path: SQUARE_PIN,
 			fillColor: '#0196FA',
 			fillOpacity: 1,
 			strokeColor: '',
-			strokeWeight: 0,
-			scale: 1/2
+			strokeWeight: 0
 		},
-		map_icon_label: '<span class="map-icon map-icon-cafe"></span>'
+		map_icon_label: '<span class="map-icon map-icon-transit-station"></span>'
 	});
 	
 	var marker3 = new Marker({
 		map: map,
 		zIndex: 30,
 		title: 'Map Icons',
-		position:  new google.maps.LatLng(-34.028249, 151.157507),
+		position:  new google.maps.LatLng(-27.46870, 153.02335),
 		icon: {
-			path: SQUARE_PIN,
+			path: MAP_PIN,
 			fillColor: '#642BB1',
 			fillOpacity: 1,
 			strokeColor: '',
-			strokeWeight: 0,
-			scale: 1/2
+			strokeWeight: 0
 		},
-		map_icon_label: '<span class="map-icon map-icon-car-wash"></span>'
+		map_icon_label: '<span class="map-icon map-icon-city-hall"></span>'
 	});
 	
 	var marker4 = new Marker({
 		map: map,
 		zIndex: 40,
 		title: 'Map Icons',
-		position:  new google.maps.LatLng(-33.80010128657071, 151.28747820854187),
+		position:  new google.maps.LatLng(-27.46900, 153.02009),
 		icon: {
-			path: SQUARE_PIN,
-			fillColor: '#00CCBB',
+			path: SQUARE_ROUNDED,
+			fillColor: '#6331AE',
 			fillOpacity: 1,
 			strokeColor: '',
-			strokeWeight: 0,
-			scale: 1/2
+			strokeWeight: 0
 		},
-		map_icon_label: '<span class="map-icon map-icon-male"></span>'
+		map_icon_label: '<span class="map-icon map-icon-courthouse"></span>'
+	});
+	
+	var marker5 = new Marker({
+		map: map,
+		zIndex: 40,
+		title: 'Map Icons',
+		position:  new google.maps.LatLng(-27.46904, 153.02754),
+		icon: {
+			path: SQUARE,
+			fillColor: '#6331AE',
+			fillOpacity: 1,
+			strokeColor: '',
+			strokeWeight: 0
+		},
+		map_icon_label: '<span class="map-icon map-icon-grocery-or-supermarket"></span>'
+	});
+	
+	var marker6 = new Marker({
+		map: map,
+		zIndex: 40,
+		title: 'Map Icons',
+		position:  new google.maps.LatLng(-27.47167, 153.02580),
+		icon: {
+			path: SHIELD,
+			fillColor: '#6331AE',
+			fillOpacity: 1,
+			strokeColor: '',
+			strokeWeight: 0
+		},
+		map_icon_label: '<span class="map-icon map-icon-police"></span>'
 	});
 };
 
